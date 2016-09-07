4f8038674db3e2cf76a77e60b8938fed07ca3893
Paul Lawitzki <p.lawitzki@gmail.com>
2016-09-07 22:26:10 +0200

    glew and freeglut added. Solution not compiling yet due to linker problems in freeglut.

--- gga.lua
+++ gga.lua
@@ -1,37 +1,123 @@
 solution "gga"
-configurations {
-   "Release",
-   "Debug"
-}
-location (_OPTIONS["to"])
+   configurations {"Release", "Debug" }
+   location (_OPTIONS["to"])
 
-project "gga"
-targetname "gga"
-language "C++"
-kind "ConsoleApp"
-flags {
-   "No64BitChecks",
-   "StaticRuntime"
-}
-includedirs {
-   "./lib"
-}
+-------------------------------------
+-- freeglut static lib
+-------------------------------------
+   project "freeglut_proj"
+   targetname "freeglut_static"
+   language "C"
+   kind "StaticLib"
+   defines {
+      "FREEGLUT_STATIC"
+   }
+   
+   includedirs {
+      "./3rdParty/freeglut/include"
+   }
 
-files {
-   "./src/**.h",
-   "./src/**.cpp"
-}
+   files {
+      "./3rdParty/freeglut/src/**.h",
+      "./3rdParty/freeglut/src/**.c"
+   }
 
-configuration "Debug"
-   defines     { "_DEBUG" }
-   flags       { "Symbols" }
+   configuration "windows"
+   defines {
+      "_WIN32",
+      "TARGET_HOST_MS_WINDOWS"
+   }
+   
+   configuration "Debug"
+      defines     { "_DEBUG" }
+      flags       { "Symbols" }
+      targetdir   "./lib/debug"
 
-configuration "Release"
-   defines     { "NDEBUG" }
-   flags       { "OptimizeSize" }
+   configuration "Release"
+      defines     { "NDEBUG" }
+      flags       { "OptimizeSize" }
+      targetdir   "./lib/release"
 
-configuration "windows"
-   targetdir   "./bin/windows"
+-------------------------------------
+-- glew static lib
+-------------------------------------
+   project "glew_proj"
+   targetname "glew"
+   language "C"
+   kind "StaticLib"
+
+   includedirs {
+      "./3rdParty/glew/include"
+   }
+
+   files {
+      "./3rdParty/glew/src/**.h",
+      "./3rdParty/glew/src/**.c",
+      "./3rdParty/glew/include/GL/**.h"
+   }
+   
+   configuration "windows"
+      targetdir "./lib"
+
+   configuration "Debug"
+      defines     { "_DEBUG" }
+      flags       { "Symbols" }
+      targetdir   "./lib/debug"
+
+   configuration "Release"
+      defines     { "NDEBUG" }
+      flags       { "OptimizeSize" }
+      targetdir   "./lib/release"
+
+-------------------------------------   
+-- top level gga project
+-------------------------------------
+   project "gga"
+   targetname "gga"
+   language "C++"
+   kind "ConsoleApp"
+   defines { "FREEGLUT_STATIC" }
+   
+   flags {
+      "No64BitChecks",
+      "StaticRuntime"
+   }
+   
+   includedirs {
+      "./lib",
+      "./3rdParty/freeglut/include",
+      "./3rdParty/glew/include"
+   }
+
+   libdirs {
+      "./lib"
+   }
+
+   links {
+      "freeglut_proj",
+      "glew_proj"
+   }
+   
+   files {
+      "./src/**.h",
+      "./src/**.cpp"
+   }
+
+   configuration "Debug"
+      defines     { "_DEBUG" }
+      flags       { "Symbols" }
+      libdirs     { "./lib/debug" }
+      links       { "glew", "freeglut_static" }
+
+   configuration "Release"
+      defines     { "NDEBUG" }
+      flags       { "OptimizeSize" }
+      libdirs     { "./lib/release" }
+      links       { "glew", "freeglut_static" }
+
+   configuration "windows"
+      defines     { "_WIN32" }
+      targetdir   "./bin/windows"
 
 if _ACTION == "clean" then
    os.rmdir("bin")
--- src/gga.cpp
+++ src/gga.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
+#include <stdio.h>
 
 #include "simulator.h"
 #include "velocityrule.h"
+#include "GL\freeglut.h"
 
 // fixes missing reference error in gcc builds
 // see: http://stackoverflow.com/questions/329059/what-is-gxx-personality-v0-for/329195#329195
 void *__gxx_personality_v0;
 
-int main()
+int main(int argc, char* argv[])
 {
+	// init GLUT and create window
+	// glutInit(&argc, argv);
+
+	// register callbacks
+
+	// enter GLUT event processing cycle
+
+
     // testing simulator
 	gga::Simulator simulator;
 	simulator.AddSimulationRule<gga::VelocityRule>();
